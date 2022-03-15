94e5128e13828d12eed5835e6858aff1382a2140
Sawan Singh <sawansingh5216977@gmail.com>
2022-03-15 16:21:31 +0530

    handling username & password changes using state

--- src/login/Login.js
+++ src/login/Login.js
@@ -1,15 +1,60 @@
-import React from "react";
+import React, { Component } from "react";
 
-function Login() {
-  return (
-    <div>
-      Username: <input type="text" name="username" />
-      <br />
-      Password: <input type="password" name="password" />
-      <br />
-      <button>LogIn</button>
-    </div>
-  );
+class Login extends Component {
+  // define the initial state in constructor for username & password
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: "",
+      password: "",
+    };
+
+    // binding the function(event) to state...
+    this.handleUsernameChange = this.handleUsernameChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+
+    // So whenever we made changes to username & password input fields the state gets updated
+    // and when state gets updated the changes reflects back in element : This is like Two way data binding
+  }
+
+  // will work as two way data binding
+  handleUsernameChange(event) {
+    console.log(event.target.value);
+    // update the state of username
+    this.setState({ username: event.target.value });
+  }
+
+  // will work as two way data binding
+  handlePasswordChange(event) {
+    console.log(event.target.value);
+    // update the state of password
+    this.setState({ password: event.target.value });
+  }
+
+  render() {
+    return (
+      <div>
+        Username:
+        <input
+          type="text"
+          name="username"
+          value={this.state.username}
+          onChange={this.handleUsernameChange}
+        />
+        <br />
+        Password:
+        <input
+          type="password"
+          name="password"
+          value={this.state.password}
+          onChange={this.handlePasswordChange}
+        />
+        <br />
+        <button>LogIn</button>
+      </div>
+    );
+  }
 }
 
 export default Login;
