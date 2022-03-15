7cd987ad51cf1f81a550acc1d30ecc77469ff2df
Sawan Singh <sawansingh5216977@gmail.com>
2022-03-15 17:34:42 +0530

    added routing link to all the components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
 import Login from "./login/Login";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./home/Home";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
