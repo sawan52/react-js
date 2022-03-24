ff95f960d16f1edf130afec76e86fe8640651bb5
Sawan Singh <sawansingh5216977@gmail.com>
2022-03-24 14:10:11 +0530

    preparing welcome page for web service call & installed axios

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-function Home(props) {
-  return (
-    <div>
-      <h1>Welcome!</h1>
-      <div className="container">
-        Welcome {props.params.name}. You can manage your TODOS{" "}
-        <Link to="/todos">here</Link>.
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.callWebService = this.callWebService.bind(this);
+  }
+
+  callWebService() {
+    console.log("webservice called");
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Welcome!</h1>
+        <div className="container">
+          Welcome {this.props.params.name}. You can manage your TODOS
+          <Link to="/todos">here</Link>.
+        </div>
+        <div className="container">
+          Click below button to see magic!!
+          <br />
+          <button onClick={this.callWebService}>Magic</button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Home;
