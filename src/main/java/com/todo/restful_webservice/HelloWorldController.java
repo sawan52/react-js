5f9b7b71d000cd7031e319f4f93771137b95453e
Sawan Singh <sawansingh5216977@gmail.com>
2022-03-27 19:25:22 +0530

    added cross origin url & path variable web service

--- src/main/java/com/todo/restful_webservice/HelloWorldController.java
+++ src/main/java/com/todo/restful_webservice/HelloWorldController.java
@@ -1,18 +1,26 @@
 package com.todo.restful_webservice;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class HelloWorldController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World!";
+        return "Hello World";
     }
 
     @GetMapping("/hello-bean")
     public HelloBean helloBean() {
         return new HelloBean("Hello World Bean!");
+    }
+
+    @GetMapping("/hello/{name}")
+    public String helloPathVariable(@PathVariable String name) {
+        return "Hello " + name;
     }
 }
