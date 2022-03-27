d2350336c3b093072aad6413fc6245c529fb29ae
Sawan Singh <sawansingh5216977@gmail.com>
2022-03-27 20:00:14 +0530

    handled error response

--- src/home/Home.js
+++ src/home/Home.js
@@ -25,7 +25,7 @@ class Home extends Component {
 
     HelloWebService.executedHelloPathVariableService(this.props.params.name)
       .then((response) => this.handleSuccessfullResponse(response))
-      .catch(() => this.handleFailedResponse());
+      .catch((error) => this.handleFailedResponse(error));
   }
 
   handleSuccessfullResponse(response) {
@@ -33,8 +33,9 @@ class Home extends Component {
     this.setState({ msg: response.data });
   }
 
-  handleFailedResponse() {
-    this.setState({ msg: "Something went wrong, please try again later." });
+  handleFailedResponse(error) {
+    console.log(error.response);
+    this.setState({ msg: error.response.data.message });
   }
 
   render() {
